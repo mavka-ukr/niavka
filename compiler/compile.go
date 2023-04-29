466fa79a66bbd63c541ef573413a13818fb1d6ad
Danik Vitek <x3665107@gmail.com>
2023-04-29 23:04:34 +0300

    Fix types assertion. Add more lang types

--- compiler/compile.go
+++ compiler/compile.go
@@ -1,39 +1,48 @@
 package compiler
 
 import (
+	"niavka/parser"
+
 	"github.com/llir/llvm/ir"
 	"github.com/llir/llvm/ir/constant"
 	"github.com/llir/llvm/ir/types"
 	"github.com/llir/llvm/ir/value"
-	"niavka/parser"
 )
 
 func getTypeByName(name string) types.Type {
-	if name == "ц32" {
+	switch name {
+	case "ц8":
+		return types.I8
+	case "ц16":
+		return types.I16
+	case "ц32":
 		return types.I32
-	} else if name == "ц64" {
+	case "ц64":
 		return types.I64
-	} else if name == "д32" {
+	case "д16":
+		return types.Half
+	case "д32":
 		return types.Float
-	} else if name == "д64" {
+	case "д64":
 		return types.Double
-	} else if name == "текст" {
-		return types.NewPointer(types.I8)
-	} else if name == "ніщо" {
+	case "логічне":
+		return types.I1
+	case "текст":
+		return types.I8Ptr
+	case "ніщо":
 		return types.Void
+	default:
+		return nil
 	}
-
-	return nil
 }
 
 func compileBody(block *ir.Block, body []interface{}) {
 	for _, element := range body {
-		switch element.(type) {
+		switch element := element.(type) {
 		case parser.AssignNode:
 			panic("not implemented")
 		case parser.CallNode:
-			call := element.(parser.CallNode)
-			compileCall(block, call)
+			compileCall(block, element)
 		}
 	}
 }
@@ -47,9 +56,8 @@ func compileCall(block *ir.Block, call parser.CallNode) {
 	}
 	var args []value.Value
 	for _, arg := range call.Args {
-		switch arg.(type) {
+		switch stringNode := arg.(type) {
 		case parser.StringNode:
-			stringNode := arg.(parser.StringNode)
 			hello := constant.NewCharArrayFromString(stringNode.Value + "\x00")
 			str := block.Parent.Parent.NewGlobalDef("str", hello)
 			zero := constant.NewInt(types.I64, 0)
@@ -65,9 +73,8 @@ func CompileProgramNode(program parser.ProgramNode) *ir.Module {
 	m.NewFunc("puts", types.I32, ir.NewParam("", types.NewPointer(types.I8)))
 
 	for _, element := range program.Elements {
-		switch element.(type) {
+		switch diia := element.(type) {
 		case parser.DiiaNode:
-			diia := element.(parser.DiiaNode)
 			var diiaFuncParams []*ir.Param
 			for _, param := range diia.Params {
 				diiaFuncParams = append(diiaFuncParams, ir.NewParam(param.Name, getTypeByName(param.Type)))
